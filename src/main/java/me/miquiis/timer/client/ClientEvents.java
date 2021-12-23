ae399894f57ff5001975bb44fca6a6bd14fa6903
BuildTools <unconfigured@null.spigotmc.org>
2021-12-23 17:16:13 -0600

    Initial Commit

---
+++ src/main/java/me/miquiis/timer/client/ClientEvents.java
@@ -0,0 +1,33 @@
+package me.miquiis.timer.client;
+
+import me.miquiis.timer.Timer;
+import me.miquiis.timer.common.util.RenderUtil;
+import net.minecraft.client.MainWindow;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Timer.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ClientEvents {
+
+    private static String text = "Test Text";
+    private static int color = 0xFFFFFF;
+    private static FontRenderer fontRenderer;
+
+    private static void verifyRenderer() {
+        if (fontRenderer != null) return;
+        fontRenderer = Minecraft.getInstance().fontRenderer;
+    }
+
+    @SubscribeEvent
+    public static void render(RenderGameOverlayEvent.Text event)
+    {
+        verifyRenderer();
+        RenderUtil.renderTextWithShadow(event.getMatrixStack(), fontRenderer, event.getWindow(), "Test", 0, 50, 2, color);
+    }
+
+}
---
+++ src/main/java/me/miquiis/timer/common/util/RenderUtil.java
@@ -0,0 +1,19 @@
+package me.miquiis.timer.common.util;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.MainWindow;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.util.text.StringTextComponent;
+
+public class RenderUtil {
+
+    public static void renderTextWithShadow(MatrixStack stack, FontRenderer fontRenderer, MainWindow window, String text, float xOffset, float yOffset, float scale, int color)
+    {
+        scale /= window.getGuiScaleFactor();
+        stack.push();
+        stack.scale(scale, scale, scale);
+        fontRenderer.drawTextWithShadow(stack, new StringTextComponent(text), (((window.getScaledWidth()) / 2f / scale) - fontRenderer.getStringWidth(text) / 2f) + xOffset, (((window.getScaledHeight()) / 2f / scale) - fontRenderer.FONT_HEIGHT / 2f) + yOffset, color);
+        stack.pop();
+    }
+
+}
